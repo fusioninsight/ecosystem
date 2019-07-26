c49f94d74aab9aacf6a23891970b7f56b08fe560
fengyunyx <fengyunyx@gmail.com>
2019-07-26 17:22:27 +0800

    update mkdocs

--- README.md
+++ README.md
@@ -2,5 +2,5 @@
 
 Please choose your Languages
 
-* [English](en/)
-* [中文](zh-hans/)
+* [English](https://fusioninsight.github.io/ecosystem/en/)
+* [中文](https://fusioninsight.github.io/ecosystem/zh-hans/)
---
+++ zh-hans/js/baidu-tongji.js
@@ -0,0 +1,7 @@
+var _hmt = _hmt || [];
+(function() {
+  var hm = document.createElement("script");
+  hm.src = "https://hm.baidu.com/hm.js?4a116571a16f57f837e98770d8aebc70";
+  var s = document.getElementsByTagName("script")[0];
+  s.parentNode.insertBefore(hm, s);
+})();
